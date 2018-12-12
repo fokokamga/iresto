46a53c1d1d0620fda489b25fd40c761c5614e36c
Florin KAMGA <fokokamga@yahoo.fr>
2018-12-12 19:21:11 +0100

    config en production

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$config['base_url'] = 'http://localhost/iresto/';
+//$config['base_url'] = 'http://localhost/iresto/'; //en dev
+$config['base_url'] = 'https://iresto.000webhostapp.com/iresto/'; // en production
 
 /*
   |--------------------------------------------------------------------------
